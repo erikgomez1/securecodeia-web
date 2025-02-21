f270fdbd601897fb40e290da225a0d0dca8ea25b
erikgomez1 <egomcor1211@g.educaand.es>
2025-02-21 19:04:16 +0100

    Version 1.0
    Version primera

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// Script para el slider de la página principal
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.opacity = "0";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.opacity = "1";
+    setTimeout(showSlides, 3000); // Cambia cada 3 segundos
+}
